195d091ca63894eb9e4dd4e0a7e2137f1c0d18d5
AshParker19 <ashparker4219@gmail.com>
2024-02-16 14:22:13 +0000

    added a validation of the player in the map

--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: anshovah <anshovah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 19:17:51 by anshovah          #+#    #+#             */
-/*   Updated: 2024/02/11 20:40:03 by anshovah         ###   ########.fr       */
+/*   Updated: 2024/02/16 14:18:14 by anshovah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,26 @@ char    *skip_empty_lines(int cf_fd)
     return (line);
 }
 
-bool    read_map(t_cub *cub, int cf_fd)
+bool    validate_player(char *str)
+{
+    bool    found;
+    int     i;
+
+    found = false;
+    i = -1;
+    while (str[++i])
+    {
+        if (str[i] == 'N' || str[i] == 'W' || str[i] == 'E' || str[i] == 'S')
+        {
+            if (found)
+                return (false);
+            found = true;
+        }
+    }
+    return (true);
+}
+
+char    *read_map(t_cub *cub, int cf_fd)
 {
     char    *long_line;
     char    *cur_line;
@@ -75,33 +94,97 @@ bool    read_map(t_cub *cub, int cf_fd)
     cur_line = NULL;
     long_line = skip_empty_lines(cf_fd);
 	if (!long_line)
-		return (false);
+		return (NULL);
     while (long_line)
     {
         cur_line = gnl(cf_fd);
-        if (!cur_line)
+        if (!cur_line || is_line_empty(cur_line))
+        {
+            free_whatever("p", cur_line);
             break ;
-        if (!is_line_valid(cur_line) || is_line_empty(cur_line))
+        }
+        if (!is_line_valid(cur_line))
         {
             free_whatever("pp", long_line, cur_line);
-            return (false);
+            return (NULL);
         }
         replace_spaces(cur_line);
         long_line = append_str(long_line, cur_line, true);
     }
-    cub->map_config.map = ft_split(long_line, '$');
-    if (!cub->map_config.map)
-        return (false);
-	free_whatever("p", long_line);
-    return (true);
+    return (long_line);
 }
 
 bool    parse_map(t_cub *cub, int cf_fd)
 {
-    if (!read_map(cub, cf_fd))
+    char    *map_line;
+
+    map_line = read_map(cub, cf_fd);  
+    if (!map_line)
         return (false);
-    int i = -1;
-    while (cub->map_config.map[++i])
-        printf ("%s\n", cub->map_config.map[i]);
+    if (!validate_player(map_line))
+    {
+        free_whatever("p", map_line);
+        return (false);
+    }
+    cub->map_config.map = ft_split(map_line, '$');
+    free_whatever("p", map_line);
+    if (!cub->map_config.map)
+        return (false);
+    printf ("CORRECT\n");
     return (true);
 }
+
+// int i = -1;
+// int i = -1;
+// while (map[++i])
+//    j = -1;
+//    while (map[i][++j]
+// nt i = -1;
+// while (map[++i])
+//    j = -1;
+//    while (map[i][++j]
+//   {
+
+
+//    }
+// int i = -1;
+// while (map[++i])
+//    j = -1;
+//    while (map[i][++j]
+//   {
+//        if (map[i][++j] != ' ' || map[i][++j] != '1'
+//               check all neighbor cells (top, bottom, left, right)
+//    }
+// nt i = -1;
+// while (map[++i])
+//    j = -1;
+//    while (map[i][++j]
+//   {
+//        if (map[i][++j] != ' ' || map[i][++j] != '1' 
+//              if (i == 0 || !map[i][j] || i == map hight - 1 || j == 0
+//                       its not valid
+//               check all neighbor cells (top, bottom, left, right)
+//    }
+// int i = -1;
+// while (map[++i])
+//    j = -1;
+//    while (map[i][++j]
+//   {
+//        if (map[i][++j] != ' ' || map[i][++j] != '1' 
+//            continue ;
+//        if (i == 0 || !map[i][j] || i == map hight - 1 || j == 0
+//                  its not valid
+//             check all neighbor cells (top, bottom, left, right)
+//    }
+// the last one id the right one
+// int i = -1;
+// while (map[++i])
+//    j = -1;
+//    while (map[i][++j]
+//   {
+//        if (map[i][++j] != ' ' || map[i][++j] != '1' 
+//            continue ;
+//        if (i == 0 || !map[i][j + 1] || i == map hight - 1 || j == 0
+//                  its not valid
+//             check all neighbor cells (top, bottom, left, right)
+//    }
